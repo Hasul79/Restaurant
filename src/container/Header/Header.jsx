333798a5b885807bc5112dadef1d49e5e129ad38
Hasul79 <hatuk2@mail.ru>
2022-11-20 00:11:45 +0400

    add hamburger bar

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-
 import './Header.css';
+import { images} from '../../constants';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app_header app_wrapper section_padding' id="home">
+   <div className='app_wrapper_info'>
+      <SubHeading />
+   </div>
+   <div className='app_wrapper_img'>
+
+   </div>
   </div>
 );
 
