dd00a9aded765fce5ecf91f352df4f7e4b80403e
Hasul79 <hatuk2@mail.ru>
2022-11-20 18:29:14 +0400

    home

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -4,12 +4,16 @@ import { images} from '../../constants';
 import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div className='app_header app_wrapper section_padding' id="home">
-   <div className='app_wrapper_info'>
-      <SubHeading />
+  <div className='app__header app__wrapper section__padding' id="home">
+   <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new flavour" />
+      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{margin:'2rem 0'}}>Restaurant (Latin: restauro)
+      <br /> a catering establishment with a wide variety of difficult-to-prepare dishes, including take-out and signature dishes.</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
    </div>
-   <div className='app_wrapper_img'>
-
+   <div className='app__wrapper_img'>
+    <img src={images.welcome} />
    </div>
   </div>
 );
