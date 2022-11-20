0efe80a9014fd667a56c6d022958ed01a51b2bd4
Hasul79 <hatuk2@mail.ru>
2022-11-21 00:13:08 +0400

    about

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import './Header.css';
-import { images} from '../../constants';
+
 import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import './Header.css';
 
 const Header = () => (
-  <div className='app__header app__wrapper section__padding' id="home">
-   <div className='app__wrapper_info'>
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
-      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
-      <p className="p__opensans" style={{margin:'2rem 0'}}>Restaurant (Latin: restauro)
-      <br /> a catering establishment with a wide variety of difficult-to-prepare dishes, including take-out and signature dishes.</p>
-      <button type='button' className='custom__button'>Explore Menu</button>
-   </div>
-   <div className='app__wrapper_img'>
-    <img src={images.welcome} />
-   </div>
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
-
 import './SpecialMenu.css';
+import { SubHeading , MenuItem } from '../../components';
+import { images, data } from '../../constants';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+      <div className="app__specialMenu-title">
+        <SubHeading  title="Menu that fits you palatte"/>
+        <h1 className="headtext__cormorant"> Today's Special</h1>
+     </div> 
+     {/* wine menu & beer */}
+     <div className='app__specialMenu-menu'>
+       <div className='app__specialMenu-menu_wine  flex__center'>
+          <p className='app__specialMenu_heading'>Wine & Beer</p>
+          <div className="app__spacialMenu-menu_items">
+            {data.wines.map((wine, index) => (
+              <p>{wine.title}</p>
+            ))}
+          </div>
+       </div>
+
+       {/* wine image */}
+
+       <div className='app__specialMenu-menu_img'>
+          <img src={images.menu}  alt="menu img" />
+       </div>
+     
+       {/* Cocktails */}
+       <div className='app__specialMenu-menu_cocktails  flex__center'>
+          <p className='app__specialMenu_heading'>Cocktails</p>
+          <div className="app__spacialMenu-menu_items">
+            {data.cocktails.map((cocktail, index) => (
+              <p>{cocktail.title}</p>
+            ))}
+          </div>
+       </div>
+
+     </div>
   </div>
 );
 
