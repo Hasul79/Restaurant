9e3b16ad30c00ba708068a2d784eaf3499a3da26
Hasul79 <hatuk2@mail.ru>
2022-11-21 11:12:30 +0400

    add menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -9,13 +9,17 @@ const SpecialMenu = () => (
         <SubHeading  title="Menu that fits you palatte"/>
         <h1 className="headtext__cormorant"> Today's Special</h1>
      </div> 
+
      {/* wine menu & beer */}
      <div className='app__specialMenu-menu'>
        <div className='app__specialMenu-menu_wine  flex__center'>
-          <p className='app__specialMenu_heading'>Wine & Beer</p>
+          <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
           <div className="app__spacialMenu-menu_items">
             {data.wines.map((wine, index) => (
-              <p>{wine.title}</p>
+              <MenuItem  key={wine.title + index} 
+                       title={wine.title} 
+                       price={wine.price}
+                        tags={wine.tags}/>
             ))}
           </div>
        </div>
@@ -28,14 +32,19 @@ const SpecialMenu = () => (
      
        {/* Cocktails */}
        <div className='app__specialMenu-menu_cocktails  flex__center'>
-          <p className='app__specialMenu_heading'>Cocktails</p>
+          <p className='app__specialMenu-menu_heading'>Cocktails</p>
           <div className="app__spacialMenu-menu_items">
             {data.cocktails.map((cocktail, index) => (
-              <p>{cocktail.title}</p>
+              <MenuItem  key={cocktail.title + index} 
+                       title={cocktail.title} 
+                       price={cocktail.price} 
+                        tags={cocktail.tags}/>
             ))}
           </div>
        </div>
-
+     </div>
+     <div style={{marginTop:'15px'}}>
+      <button type="button" className='custom__button'>View More</button>
      </div>
   </div>
 );
